9fa6495ae53e08919df896e8e50cae337f60a5d6
rapterxcode <phonepasit9@gmail.com>
2024-09-02 08:34:51 +0700

    main

--- setup.sh
+++ setup.sh
@@ -12,7 +12,17 @@ harden_system() {
     sudo ufw allow http
     sudo ufw allow https
     sudo ufw allow 6443/tcp
-    sudo ufw allow 6443/udp
+    sudo ufw allow 179/tcp
+    sudo ufw allow 4789/tcp
+    sudo ufw allow 5473/tcp
+    sudo ufw allow 443/tcp
+    sudo ufw allow 6443/tcp
+    sudo ufw allow 2379/tcp
+    sudo ufw allow 4149/tcp
+    sudo ufw allow 10250/tcp
+    sudo ufw allow 10255/tcp
+    sudo ufw allow 10256/tcp
+    sudo ufw allow 9099/tcp
     sudo ufw enable
     sudo systemctl enable fail2ban
     sudo systemctl start fail2ban
